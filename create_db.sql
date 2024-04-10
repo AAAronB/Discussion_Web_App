9a70a1e463584e43631182be658a5ed2e80e2869
Aaron <aaronbankole12@gmail.com>
2024-04-10 03:21:36 +0100

    first commit

---
+++ create_db.sql
@@ -0,0 +1,16 @@
+--Create database script for Berties books
+
+--Create the database
+CREATE DATABASE myForum;
+USE myForum;
+
+--Create the tables
+CREATE TABLE Users (id INT AUTO_INCREMENT,
+first_name VARCHAR(50),last_name VARCHAR(50), username VARCHAR(20), password VARCHAR(20),
+email VARCHAR(50),PRIMARY KEY(id));
+CREATE TABLE Post (id INT AUTO_INCREMENT,name VARCHAR(50),
+topic VARCHAR(50),username VARCHAR(20),content VARCHAR(2000),PRIMARY KEY(id),FOREIGN KEY (username) REFERENCES Users(username));
+--Create the app user and give it access to the database
+CREATE USER 'appuser'@'localhost' IDENTIFIED WITH mysql_native_password BY 'app2027';
+GRANT ALL PRIVILEGES ON myForum.* TO 'appuser'@'localhost';
+
---
+++ insert_test_data.sql
@@ -0,0 +1,4 @@
+--Insert data into the tables
+USE myForum;
+INSERT INTO Post (name,topic,username,content)VALUES('Is MW3 overrated?','MW3','AaronB','MW3 sucks.');
+INSERT INTO Users (first_name,last_name,username,password,email)VALUES('Aaron','Bankole','AaronB','AB','aaronbankole12@gmail');
